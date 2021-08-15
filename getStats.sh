286fb24931b57c04e1f2d5cc49484d746d61ea48
jdim21 <jdim21@gmail.com>
2021-08-15 12:32:48 -0400

    find one ofs

--- getStats.sh
+++ getStats.sh
@@ -8,3 +8,12 @@ ls images/*.?*_*_*_*_* | wc | awk '{print $1}'
 
 echo "Number of zero attribute rolls: "
 ls images/*.?_____.* | wc | awk '{print $1}'
+
+echo "Diamond necklace: "
+ls images/*.??d*
+
+echo "Diamond bone: "
+ls images/*.*d??.*
+
+echo "Crown: "
+ls images/*.*c?.*
